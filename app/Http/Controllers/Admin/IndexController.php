40f9d7108997d762fe790bc318d88fe637e17ccf
cfun <1606548133@qq.com>
2019-06-24 15:47:52 +0800

    优化

--- app/Http/Controllers/Admin/IndexController.php
+++ app/Http/Controllers/Admin/IndexController.php
@@ -17,12 +17,6 @@ use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
-
-    public function __construct()
-    {
-        $this->middleware('auth:admin');
-    }
-
     public function add(Request $request)
     {
         $content = $request->input('test-editormd-html-code');
--- routes/admin.php
+++ routes/admin.php
@@ -1,18 +1,18 @@
 <?php
 
-Route::group(['namespace' => 'Admin'], function () {
+Route::group(['middleware' => 'auth:admin','namespace' => 'Admin'], function () {
 
+        //登录
         Route::get('login', 'AuthController@login')->name('login');
+        //注册
         Route::post('register', 'AuthController@register');
         Route::post('logout', 'AuthController@logout');
         Route::post('refresh', 'AuthController@refresh');
+        //文章添加
+        Route::get('add', ['uses' => 'IndexController@add', 'as' => 'admin.add']);
+        //文章编辑
+        Route::get('edit', ['uses' => 'IndexController@edit', 'as' => 'admin.edit']);
 });
 
 
-Route::group(['namespace' => 'Admin'], function () {
-
-    Route::get('add', ['uses' => 'IndexController@add', 'as' => 'admin.add']);
-
-    Route::get('edit', ['uses' => 'IndexController@edit', 'as' => 'admin.edit']);
-});
 
