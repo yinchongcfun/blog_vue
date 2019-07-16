7a68df880f19a8a9dde9af58ef991659ca9735bf
cfun <1606548133@qq.com>
2019-07-16 13:50:02 +0800

    post

--- routes/admin.php
+++ routes/admin.php
@@ -12,7 +12,7 @@ Route::group(['namespace' => 'Admin'], function () {
     //文章添加
     Route::get('phpinfo', ['uses' => 'IndexController@phpInfo', 'as' => 'admin.phpinfo']);
     //文章添加
-    Route::get('add', ['uses' => 'IndexController@add', 'as' => 'admin.add']);
+    Route::post('add', ['uses' => 'IndexController@add', 'as' => 'admin.add']);
     //文章编辑
     Route::get('detail', ['uses' => 'IndexController@detail', 'as' => 'admin.detail']);
 });
