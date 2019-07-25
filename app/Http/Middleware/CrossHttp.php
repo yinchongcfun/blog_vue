7b0bd0f94cd822b2bfabcd3345d0070f76737653
cfun <1606548133@qq.com>
2019-07-25 13:49:31 +0800

    增加与监测

--- app/Http/Middleware/CrossHttp.php
+++ app/Http/Middleware/CrossHttp.php
@@ -19,9 +19,6 @@ class CrossHttp
         $response->header('Access-Control-Allow-Origin', '*');
         $response->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Cookie, Accept');
         $response->header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, OPTIONS');
-        if(strtoupper($_SERVER['REQUEST_METHOD'])== 'OPTIONS'){
-            exit;
-        }
         $response->header('Access-Control-Allow-Credentials', 'false');
         return $response;
     }
