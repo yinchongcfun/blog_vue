44ccad7281f362df592a1f0647e7950e5ea674e1
cfun <1606548133@qq.com>
2019-08-19 09:37:34 +0800

    上传图片

--- routes/api.php
+++ routes/api.php
@@ -6,5 +6,5 @@ Route::group(['namespace' => 'Api'], function () {
 
     Route::get('tag', ['uses' => 'IndexController@tag', 'as' => 'blog.tag']);
 
-    Route::get('uploadimg', ['uses' => 'IndexController@uploadImg', 'as' => 'blog.uploadimg']);
+    Route::post('uploadimg', ['uses' => 'IndexController@uploadImg', 'as' => 'blog.uploadimg']);
 });
